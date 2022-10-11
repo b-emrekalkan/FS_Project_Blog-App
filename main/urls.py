d3b86462030df832a655054ede9f1ae19114b4c1
bemrekalkan <bemre.kalkan@gmail.com>
2022-10-11 14:28:53 +0300

    user authentication done

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,21 @@
+"""main URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
-# Three modules for swagger:
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -9,25 +24,22 @@ schema_view = get_schema_view(
     openapi.Info(
         title="BlogApp API",
         default_version="v1",
-        description="BlogApp API provides blog post info",
+        description="Blog Aplications API project provides blog post info",
         terms_of_service="#",
-        contact=openapi.Contact(
-            email="bayramemrekalkan@gmail.com"),  # Change e-mail on this line!
+        # Change e-mail on this line!
+        contact=openapi.Contact(email="bayramemrekalkan@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
 )
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include('user.urls')),
 
-    #! Url paths for swagger 👇:
-    path("swagger(<format>\.json|\.yaml)",
-         schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0),
-         name="schema-swagger-ui"),
-    path("redoc/", schema_view.with_ui("redoc",
-         cache_timeout=0), name="schemaredoc"),
-
-    path('auth/', include('dj_rest_auth.urls'))
+    #! swagger urls 👇
+    path("swagger(<format>\.json|\.yaml)", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schemaredoc"),   
 ]
--- user/views.py
+++ user/views.py
@@ -1,10 +1,13 @@
 from rest_framework import generics, status
+from django.conf import settings
+from user.serializers import RegisterSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from .serializers import RegisterSerializer
+from user.models import User
 from django.contrib.auth import get_user_model
-
+# User = settings.AUTH_USER_MODEL
 User = get_user_model()
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -15,11 +18,10 @@ class RegisterView(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         data = serializer.data
-        if Token.objects.filter(user=user).exists():
+        if Token.objects.filter(user=user):
             token = Token.objects.get(user=user)
             data['token'] = token.key
         else:
-            data['error'] = 'User dont have token. Please login'
+            data['error'] = 'User does not have token . Try again ...'
         headers = self.get_success_headers(serializer.data)
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
-
